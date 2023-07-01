2c135a6eafd268891d1602dac74e2f5e8d4aff6d
Ash <126411621+Bunnyy0905@users.noreply.github.com>
2023-07-01 23:58:03 +0530

    Add files via upload

---
+++ pc_001.py
@@ -0,0 +1,22 @@
+"""
+
+class Comps:
+    pass
+
+stu1=Comps()
+stu2=Comps()
+# Note: An Object is an instance of Class. Therefore, creating an object is also known as Instancination
+print(type(stu1))
+
+
+"""
+
+
+class computer: #defining/creating a class namely computer
+    def config(self):#defining a method/function
+        print("This is the method/functions of the class computer")
+
+comp1=computer()    #definiing an object-->comp1 of class computer
+# print(type(comp1))
+
+
